29124a1ed0d58b3985576ccae03ed2bb3034904f
yesser <yasser.rebai@yassir.com>
2023-04-10 01:46:47 +0100

    added comment

--- src/middleware/isLogged.ts
+++ src/middleware/isLogged.ts
@@ -1,34 +1,29 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
-import Users from '../models/user/userModel';
-
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import Users from "../models/user/userModel";
 
 const auth = async (req: JwtPayload, res: Response, next: NextFunction) => {
-    try {
-        const token: string | undefined = req.header("Authorization");
+  try {
+    const token: string | undefined = req.header("Authorization");
 
-        if(!token){
-            return res.status(400).json({ msg: "You are not authorized" });
-        }
-
-        const decoded: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-        if (!decoded) {
-          return res.status(400).json({ msg: "You are not authorized" });
-        }
-
-        const user = await Users.findOne({_id: decoded.id});
-
-        req.user = user;
-        next();
-    } catch (err) {
-        return res.status(500).json({msg: err.message});
+    if (!token) {
+      return res.status(400).json({ msg: "You are not authorized" });
     }
-}
+
+    const decoded: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    if (!decoded) {
+      return res.status(400).json({ msg: "You are not authorized" });
+    }
+
+    const user = await Users.findOne({ _id: decoded.id });
+
+    req.user = user;
+    next();
+    // rather than binding the whole document we should bind _id or email only
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
 
 export default auth;
-
-
-
-
-
