7a5afda53bca32fc0a787b71e28a63e5ae15e771
yesser <yasser.rebai@yassir.com>
2023-04-14 17:38:49 +0100

    added login user

--- src/features/users/user.controller.ts
+++ src/features/users/user.controller.ts
@@ -3,6 +3,7 @@ import { Controller } from '../../shared';
 import UserService from './user.service';
 import validationMiddleware from '../../middlewares/dataValidator';
 import RegisterUserDto from './dtos/registerUser.dto';
+import LoginUserDto from './dtos/loginUser.dto';
 
 export default class UserController implements Controller {
   path = '/users';
@@ -17,11 +18,21 @@ export default class UserController implements Controller {
       validationMiddleware(RegisterUserDto),
       this.registerUser,
     );
+    this.route.post(
+      '/login',
+      validationMiddleware(LoginUserDto),
+      this.loginUser,
+    );
   }
 
   async registerUser(req: Request, res: Response): Promise<void> {
     const registerUser: RegisterUserDto = req.body;
     const result = await UserService.registerUser(registerUser);
     res.status(201).send(result);
+  }
+  async loginUser(req: Request, res: Response): Promise<void> {
+    const loginCred: LoginUserDto = req.body;
+    const result = await UserService.loginUser(loginCred);
+    res.status(200).send(result);
   }
 }
